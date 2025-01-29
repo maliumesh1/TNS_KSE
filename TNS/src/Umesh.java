c99f5f87da6e00c54cd69ace5f1a2c838d9cbb77
Umesh Mali <um4319167@gmail.com>
2025-01-29 14:24:05 +0530

    21

--- TNS/src/Umesh.java
+++ TNS/src/Umesh.java
@@ -3,6 +3,11 @@ public class Umesh {
 
 	public static void main(String[] args) {
 		System.out.print("System.out.print(\"\");");
+		
+		for(int i=0;i<15;i++)
+		{
+			System.out.print(i);
+		}
 
 	}
 
